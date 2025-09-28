dd24bb915855841d6e791c7f70bdadb4f004abba
gegxkss <ksyusha.lge@gmail.com>
2025-09-28 13:38:39 +0300

    full service functionality

--- main.go
+++ main.go
@@ -7,15 +7,12 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/gegxkss/wbL0/internal/cache"
 	"github.com/gegxkss/wbL0/internal/config"
 	"github.com/gegxkss/wbL0/internal/handlers"
-	"github.com/gegxkss/wbL0/internal/models"
 	"github.com/gegxkss/wbL0/kafka"
 	"github.com/gegxkss/wbL0/migrations"
-	"gorm.io/gorm"
 )
 
 var migrate = flag.Bool("m", false, "Run database migrations")
@@ -25,7 +22,7 @@ const (
 	groupID = "orders-group"
 )
 
-var kafkaAddresses = []string{"localhost:9091", "localhost:9092"}
+var kafkaAddresses = []string{"localhost:9091", "localhost:9092", "localhost:9093"}
 
 func main() {
 	flag.Parse()
@@ -41,8 +38,6 @@ func main() {
 
 	cache := cache.NewCache()
 	log.Printf("Cache initialized (empty)")
-
-	go simulateOrders(cache, config.DB) //////////////
 
 	consumer, err := kafka.NewConsumer(kafkaAddresses, topic, groupID, config.DB, cache)
 	if err != nil {
@@ -69,54 +64,6 @@ func main() {
 	log.Println("HTTP API is available on http://localhost:8081")
 
 	waitForShutdown()
-}
-func simulateOrders(cache *cache.Cache, db *gorm.DB) {
-	time.Sleep(5 * time.Second)
-
-	log.Println("Creating demo orders for testing...")
-	testOrder1 := models.Order{
-		OrderUID:          "b563feb7b2b84b6test",
-		TrackNumber:       "WBILMTESTTRACK",
-		Entry:             "WBIL",
-		Locale:            "en",
-		InternalSignature: "",
-		CustomerId:        "test",
-		DeliveryService:   "meest",
-		ShardKey:          "9",
-		SmId:              99,
-		DateCreated:       time.Now(),
-		OofShard:          "1",
-	}
-
-	if err := cache.Set(testOrder1.OrderUID, &testOrder1); err != nil {
-		log.Printf("Failed to add demo order to cache: %v", err)
-	} else {
-		log.Printf("Demo order 1 created: %s", testOrder1.OrderUID)
-	}
-
-	testOrder2 := models.Order{
-		OrderUID:          "test-order-456",
-		TrackNumber:       "TESTTRACK456",
-		Entry:             "TEST",
-		Locale:            "ru",
-		InternalSignature: "",
-		CustomerId:        "demo",
-		DeliveryService:   "pochta",
-		ShardKey:          "5",
-		SmId:              50,
-		DateCreated:       time.Now(),
-		OofShard:          "2",
-	}
-
-	if err := cache.Set(testOrder2.OrderUID, &testOrder2); err != nil {
-		log.Printf("Failed to add demo order to cache: %v", err)
-	} else {
-		log.Printf("Demo order 2 created: %s", testOrder2.OrderUID)
-	}
-
-	log.Println("Demo orders ready:")
-	log.Println("   - b563feb7b2b84b6test")
-	log.Println("   - test-order-456")
 }
 
 func waitForShutdown() {
